349ad767f1eed6ac72b3c884c996a6e5711f9845
hovvardhughes <hovvardhughes@gmail.com>
2021-12-04 17:22:00 +0200

    In progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,51 @@
 #include <Arduino.h>
 #include "OneButton.h"
 
-#define PIN_INPUT 23
-#define PIN_LED 17
+#define PIN_INPUT 19
+#define PIN_LED 15
 #define COM_PORT_SPEED 115200
-#define DELAY_IN_MILLIS 5000
-#define DELAY_COMMAND  -1
+#define DELAY_IN_MILLIS 1000
+#define DELAY_COMMAND -1
 
 class PowerRelay
 {
 public:
   int order;
   int iONumber;
-  int state;
+  bool writable;
 
-  PowerRelay(int orderArg, int iONumberArg, int stateArg)
+  PowerRelay(int orderArg, int iONumberArg)
   {
-
     order = orderArg;
     iONumber = iONumberArg;
-    state = stateArg;
+    writable = true;
   }
 
-  void write(int newState)
+  void writeState(int newState)
   {
-    state = newState;
-    digitalWrite(iONumber, newState);
+    if (writable)
+      digitalWrite(iONumber, newState);
+  }
+
+  bool readState()
+  {
+    return digitalRead(iONumber);
   }
 };
 
-int ledState = HIGH;
 bool power = false;
 
 #define POWER_RELAY_COUNT 5
 PowerRelay allPowerRelays[POWER_RELAY_COUNT] = {
-    PowerRelay(0, 25, LOW),
-    PowerRelay(1, 26, LOW),
-    PowerRelay(2, 27, LOW),
-    PowerRelay(3, 14, LOW),
-    PowerRelay(4, 12, LOW)};
+    PowerRelay(0, 25),
+    PowerRelay(1, 26),
+    PowerRelay(2, 27),
+    PowerRelay(3, 14),
+    PowerRelay(4, 12)};
 
 OneButton button(PIN_INPUT, true);
 
-// 1.
-// Single Press = MAIN PWRON/OFF, Usual operation
-// ON - 1+2+4..timer..3+5	OFF - 3+5..timer..1+2+4
-// 2.
-// Double Press = Stand-By ON/OFF
-// ON - 2+4..timer..3	OFF - 3..timer..2+4     5=>previous state
-// 3.
-// Long Press = Indicaion OFF
-// OFF - 5..timer..4
-// Must work only on power-off mode, to prevent sparking!
-// (Power-on requires power cycling for whole system.)
-
-
-void doubleClick()
-{
-  Serial.println("x2");
-
-  ledState = !ledState; // reverse the LED
-  digitalWrite(PIN_LED, ledState);
-}
-
-
-void setPowerToRelays(int relayIndexesAndDelayCommands[], int size, int state)
+static void setPowerToRelays(int relayIndexesAndDelayCommands[], int size, int state)
 {
   for (size_t i = 0; i < size; i++)
   {
@@ -77,11 +58,23 @@ void setPowerToRelays(int relayIndexesAndDelayCommands[], int size, int state)
     }
 
     PowerRelay powerRelay = allPowerRelays[relayIndex];
-    powerRelay.write(state);
+    powerRelay.writeState(state);
   }
 }
 
-void onButtonClick()
+void onDoubleClick()
+{
+  if (!power)
+    return;
+
+  bool state = allPowerRelays[2].readState();
+  int indexesToPowerOffRelaysAndDelayCommands[] = {2, DELAY_COMMAND, 1, 3};
+  int indexesToPowerOnRelaysAndDelayCommands[] = {1, 3, DELAY_COMMAND, 2};
+
+  setPowerToRelays(state ? indexesToPowerOffRelaysAndDelayCommands : indexesToPowerOnRelaysAndDelayCommands, 4, !state);
+}
+
+void onClick()
 {
   if (!power)
     power = true;
@@ -92,7 +85,20 @@ void onButtonClick()
   int indexesToPowerOffRelaysAndDelayCommands[] = {2, 4, DELAY_COMMAND, 0, 1, 3};
   int size = POWER_RELAY_COUNT + 1;
 
-  setPowerToRelays(power ? indexesToPowerOnRelaysAndDelayCommands : indexesToPowerOffRelaysAndDelayCommands, size, int(power));
+  setPowerToRelays(power ? indexesToPowerOnRelaysAndDelayCommands : indexesToPowerOffRelaysAndDelayCommands, size, power);
+}
+
+void onLongPressStart()
+{
+  if (!power)
+    return;
+
+  PowerRelay &powerRelay = allPowerRelays[4];
+  if (powerRelay.readState())
+  {
+    powerRelay.writeState(LOW);
+    powerRelay.writable = false;
+  }
 }
 
 void setup()
@@ -102,7 +108,7 @@ void setup()
 
   pinMode(PIN_LED, OUTPUT); // sets the digital pin as output
 
-  digitalWrite(PIN_LED, ledState);
+  digitalWrite(PIN_LED, HIGH);
 
   for (size_t i = 0; i < POWER_RELAY_COUNT; i++)
   {
@@ -110,9 +116,9 @@ void setup()
     pinMode(powerRelay.iONumber, OUTPUT);
   }
 
-  // link the doubleclick function to be called on a doubleclick event.
-  button.attachDoubleClick(doubleClick);
-  button.attachClick(onButtonClick);
+  button.attachDoubleClick(onDoubleClick);
+  button.attachClick(onClick);
+  button.attachLongPressStart(onLongPressStart);
 }
 
 void loop()
