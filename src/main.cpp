59c48332b0d75f7466c72a3001147f3b626b63d1
hovvardhughes <hovvardhughes@gmail.com>
2022-04-29 00:45:44 +0300

    Comments was added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,53 @@
+ /* 
+ Setup:
+ * Connect a btn0 to pin 19 (ButtonPin) and ground.
+ * Connect a btn1 to pin 18 (ButtonPin) and ground.
+ * 
+ * The Serial interface is used for output the detected events. 
+ */
+
 #include <Arduino.h>
 #include "OneButton.h"
 
+/*
+GPIO Pins Corresponding:
+
+GPIO      Connected to...
+2         BTNLED1             'INPUT_SELECTOR_BUTTON_LED'
+4         SPK                 'BUZZER'
+12        PRLY4               'PowerRelay(12)'  (360VDCOUT)
+13        1WIRE               'TEMP_SENS'
+14        PRLY3               'PowerRelay(14)'  (6.3ACOUT)
+15        BTNLED0             'POWER_BUTTON_LED'
+16        VOLUME MOTOR REV
+17        VOLUME MOTOR FWD
+18        BTN1                'INPUT_SELECTOR_BUTTON'
+19        BTN0                'POWER_BUTTON'
+21        MPWRON              'MAINPOWERON'
+22        SDA                 'I2C_SDA'
+23        SCL                 'I2C_SCL'
+25        PRLY0               'PowerRelay(25)'  (230VACIN)
+26        PRLY1               'PowerRelay(26)'  (12ACOUT)
+27        PRLY2               'PowerRelay(27)'  (250VACOUT)
+32        INPTRLY0            'InputRelay(32)'  (IN1)
+33        INPTRLY1            'InputRelay(33)'  (IN2)
+36        ADC1                'SELFVIN'
+*/
+
+//BUTTONS
 #define POWER_BUTTON_PIN 19
 #define INPUT_SELECTOR_BUTTON_PIN 18
+#define MAINPOWERON_PIN 21
+//LEDS
+#define POWER_BUTTON_LED_PIN 15
+#define INPUT_SELECTOR_BUTTON_LED_PIN 2
+//OTHER
+#define BUZZER_PIN 4
 
-#define PIN_LED 15
+
 #define COM_PORT_SPEED 115200
-#define DELAY_IN_MILLIS 1000
+#define DELAY_IN_MILLIS 1000  //For Debugging
+//#define DELAY_IN_MILLIS 25000    //For Release - Pre-programmed Heating Filaments Delay
 #define DELAY_COMMAND -1
 
 #define POWER_RELAY_COUNT 5
@@ -51,8 +92,9 @@ Relay allInputRelays[] = {
 
 Relay currentInputRelay = allInputRelays[0];
 
-OneButton powerButton(POWER_BUTTON_PIN);
-OneButton inputSelectorButton(INPUT_SELECTOR_BUTTON_PIN);
+OneButton powerButton(POWER_BUTTON_PIN);  // Setup a new OneButton on pin 19
+OneButton inputSelectorButton(INPUT_SELECTOR_BUTTON_PIN); // Setup a new OneButton on pin 18
+OneButton mainPowerOnButton(MAINPOWERON_PIN); // Setup a new (virtual) OneButton on pin 21
 
 static void setPowerToRelays(int relayIndexesAndDelayCommands[], int size, int state)
 {
@@ -62,6 +104,7 @@ static void setPowerToRelays(int relayIndexesAndDelayCommands[], int size, int s
 
     if (relayIndex == DELAY_COMMAND)
     {
+      Serial.println ("Pre-programmedHeatingFilamentsBeginning...");
       delay(DELAY_IN_MILLIS);
       continue;
     }
@@ -73,6 +116,8 @@ static void setPowerToRelays(int relayIndexesAndDelayCommands[], int size, int s
 
 void onDoubleClickPowerButton()
 {
+  Serial.println("PowerButtonDoubleClick:Stand-bySequenceWasStarting...");
+
   if (!power)
     return;
 
@@ -85,6 +130,8 @@ void onDoubleClickPowerButton()
 
 void onClickPowerButton()
 {
+  Serial.println("PowerButtonClick:InitialisingPowerSequence...");
+
   if (!power)
     power = true;
   else
@@ -100,6 +147,8 @@ void onClickPowerButton()
 
 void onLongPressPowerButtonStart()
 {
+  Serial.println("PowerButtonLongpressStart:PoweringOffVU's...");
+
   if (!power)
     return;
 
@@ -113,6 +162,8 @@ void onLongPressPowerButtonStart()
 
 void onClickInputSelectorButton()
 {
+  Serial.println("InptSelectorButtonCick:SwitcingInputs...");
+
   if (!power)
     return;
 
@@ -130,13 +181,50 @@ void onClickInputSelectorButton()
   }
 }
 
+void onLongPressMainPowerOnButtonStart()
+{ 
+  Serial.println("MasterDeviceOff:PowerSequenceWasStopping...");
+     if (power)
+    power = false; 
+
+    int indexesToPowerOffRelaysAndDelayCommands[] = {2, 4, DELAY_COMMAND, 0, 1, 3};
+    int size = POWER_RELAY_COUNT + 1;
+
+  setPowerToRelays(indexesToPowerOffRelaysAndDelayCommands, size, power);
+  currentInputRelay.writeState(power);
+}
+
+void onLongPressMainPowerOnButtonStop()
+{
+  Serial.println("MasterDeviceOn:MainPowerOnSequenceWasStarting...");
+    if (!power)
+    power = true;
+
+    int indexesToPowerOnRelaysAndDelayCommands[] = {0, 1, 3, DELAY_COMMAND, 2, 4};
+    int size = POWER_RELAY_COUNT + 1;
+
+  setPowerToRelays(indexesToPowerOnRelaysAndDelayCommands, size, power);
+  currentInputRelay.writeState(power);
+}
+
 void setup()
 {
   Serial.begin(COM_PORT_SPEED);
-  Serial.println("One Button Example with polling.");
+  Serial.println("OneButton Starting...");
 
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, HIGH);
+  //Setup PULLUPS: INPUT_PULLUP - means pushbutton connected to VCC, INPUT_PULLDOWN - means pushbutton connected to GND
+  pinMode(POWER_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(INPUT_SELECTOR_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(MAINPOWERON_PIN, INPUT_PULLDOWN);
+
+  pinMode(POWER_BUTTON_LED_PIN, OUTPUT);
+ // digitalWrite(POWER_BUTTON_LED_PIN, HIGH);  //dummy
+
+  pinMode(INPUT_SELECTOR_BUTTON_LED_PIN, OUTPUT);
+ //digitalWrite(INPUT_SELECTOR_BUTTON_LED_PIN, HIGH);    //dummy
+
+  pinMode(BUZZER_PIN, OUTPUT);
+
 
   for (size_t i = 0; i < POWER_RELAY_COUNT; i++)
   {
@@ -155,10 +243,15 @@ void setup()
   powerButton.attachLongPressStart(onLongPressPowerButtonStart);
 
   inputSelectorButton.attachClick(onClickInputSelectorButton);
+
+  mainPowerOnButton.attachLongPressStart(onLongPressMainPowerOnButtonStart);
+  mainPowerOnButton.attachLongPressStop(onLongPressMainPowerOnButtonStop);
 }
 
 void loop()
 {
+  //watching the push buttons:
   powerButton.tick();
   inputSelectorButton.tick();
+  mainPowerOnButton.tick();
 }
