2f60b12dd4079e04f79cbd60d9aa5078d9503353
Mihail Mozgovoy <mihail.mozgovoy@atola.com>
2022-06-30 17:56:54 +0300

    In progress

--- src/InputSelector.h
+++ src/InputSelector.h
@@ -1,12 +1,10 @@
-#include <ControlBoardEEPROM.h>
 #include <Led.h>
 #include <Arduino.h>
+#include <Relay.h>
 
 class InputSelector
 {
 private:
-  ControlBoardEEPROM _controlBoardEEPROM = ControlBoardEEPROM();
-
   Relay _allRelays[2] = {
       Relay(MAIN_INPUT_RELAY_IO_NUMBER),
       Relay(SECONDARY_INPUT_RELAY_IO_NUMBER)};
@@ -14,24 +12,11 @@ private:
   byte _selectedRelayIONumber = _allRelays[0].iONumber;
   byte _notSelectedRelayIONumber = _allRelays[1].iONumber;
 
-  void tryReinitCurrentInputRelayFromEEPROM()
-  {
-    const byte readIONumber = _controlBoardEEPROM.readCurrentInputRelayIONumber();
-    for (size_t i = 0; i < INPUT_RELAY_COUNT; i++)
-    {
-      Relay relay = _allRelays[i];
-      if (relay.iONumber == readIONumber)
-        _selectedRelayIONumber = relay.iONumber;
-    }
-  }
-
 public:
   void setup()
   {
     for (size_t i = 0; i < INPUT_RELAY_COUNT; i++)
       _allRelays[i].setup();
-
-    tryReinitCurrentInputRelayFromEEPROM();
   }
 
   void setSelectedRelayIONumber(int iONumber)
@@ -82,8 +67,6 @@ public:
     byte temp = _selectedRelayIONumber;
     _selectedRelayIONumber = _notSelectedRelayIONumber;
     _notSelectedRelayIONumber = temp;
-
-    _controlBoardEEPROM.writeCurrentInputRelayIONumber(_selectedRelayIONumber);
 
     writeToRelay(_notSelectedRelayIONumber, LOW);
     writeToRelay(_selectedRelayIONumber, HIGH);
